ecdfcb6f2671104c7d1cf1e036ec0b6f0a8b002c
Lucas Pimenta <lucas.pimenta94@outlook.com>
2019-04-28 14:42:25 -0300

    move pages markup to spefic own file

---
+++ webapp/src/pagesMarkup.js
@@ -0,0 +1,26 @@
+const markupPedidos = `
+    <div class="header-markup">
+      <h2 class="header-markup___title">Explique o que você precisa
+        <small>Peça o orçamento grátis, online!</small>
+      </h2>
+    </div>
+`;
+
+const markupDados = `
+    <div class="header-markup">
+      <div class="header-markup___content-wrapper">
+        <img src="img/phone_call.svg" class="header-markup___header-image" alt="Ilustração de um celular vibrando.">
+        <h2 class="header-markup___header-info">
+          Estamos quase lá
+          <small>Não perca tempo ligando para vários profissionais. Preencha os dados abaixo e 
+            <strong>nós encontraremos os melhores pra você!</strong>
+          </small>
+        </h2>
+      </div>
+    </div>
+`;
+
+export default {
+  markupDados,
+  markupPedidos
+}
